d2548ed221d6e56c1fba32e430fef5295d9a3fff
Deepen Dhulla <deepen@deependhulla.com>
2022-10-10 11:29:59 +0530

    updated 03
    update spelling

--- 03-mariadb-setup.sh
+++ 03-mariadb-setup.sh
@@ -2,7 +2,7 @@
 
 MYSQLPASSVPOP=`pwgen -c -1 8`
 echo $MYSQLPASSVPOP > /usr/local/src/mariadb-mydbadmin-pass
-echo "mydbadmin password in /usr/local/src/mairadb-mydbadmin-pass"
+echo "mydbadmin password in /usr/local/src/mariadb-mydbadmin-pass"
 
 echo "GRANT ALL PRIVILEGES ON *.* TO mydbadmin@localhost IDENTIFIED BY '$MYSQLPASSVPOP'" with grant option | mysql -uroot
 mysqladmin -uroot reload
